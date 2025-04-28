2165af58d5585bbde0f8d92b877d3019612aa7e0
Hadi <969hadiahmed@gmail.com>
2025-04-28 12:31:55 +0530

    feat: Company class

--- Maps/src/index.ts
+++ Maps/src/index.ts
@@ -1,2 +1,6 @@
 import { User } from "./User.ts";
+import { Company } from "./Company.ts";
+
+console.log("User:>>",new User());
+console.log("Company:>>",new Company());
 console.log("Hi from index.ts");
